9452ec7da997a4dea259e4e7bee267ccc12faa6c
Lafert <77302257+Lafert@users.noreply.github.com>
2023-06-02 21:20:10 +0300

    e

--- 3/main.py
+++ 3/main.py
@@ -1,16 +1,4 @@
-import subprocess
+from tkinter import TK
 
-try:
-    data = subprocess.check_output('netsh wlan show profiles').decode('cp866').split('\n')
-    profiles = [i.spli(':')[1:-1] for i in data if 'all profiles' in i]
-    passwf = ''
-
-    for i in profiles:
-        results = subprocess.check_output(['netsh', 'wlan', 'show','profile', i , 'key = clear']).decode('cp866').split('\n')
-        if 'res in key' in j:
-            passwf +=f'{i} --{j split(':')[1][1:-1]}\n'
-
-    print(passwf)
-
-except Exception as ex:
-    print(f' wrong - {ex}')
+root = TK
+root.mainloop()
